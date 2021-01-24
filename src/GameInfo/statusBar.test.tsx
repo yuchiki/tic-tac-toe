12059e08d6e785fe03e1ce5893d614c3f30c82c7
yuchiki <yuki08281993@gmail.com>
2021-01-25 00:17:17 +0900

    statusBarのテストを react testing tool 準拠に書き直し

--- src/GameInfo/statusBar.test.tsx
+++ src/GameInfo/statusBar.test.tsx
@@ -1,39 +1,27 @@
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
+import { render, screen } from "@testing-library/react";
 
 import { CellState } from "../gameLogic";
 import StatusBar from "./StatusBar";
 
-let container: Element;
-beforeEach(() => {
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  if (container) {
-    unmountComponentAtNode(container);
-    container.remove();
-  }
-});
-
 describe("StatusBar", () => {
   it("should show the winner when the winner O is given", () => {
-    render(<StatusBar currentPlayer={CellState.stateO} winner={CellState.stateO} />, container);
-    expect(container.textContent).toBe("winner: O");
+    render(<StatusBar currentPlayer={CellState.stateO} winner={CellState.stateO} />,);
+    expect(screen.getByText("winner: O")).toBeInTheDocument();
   });
 
    it("should show the winner when the winner X is given", () => {
-    render(<StatusBar currentPlayer={CellState.stateO} winner={CellState.stateX} />, container);
-    expect(container.textContent).toBe("winner: X");
+    render(<StatusBar currentPlayer={CellState.stateO} winner={CellState.stateX} />);
+    expect(screen.getByText("winner: X")).toBeInTheDocument();
   });
 
   it("should show the current player O when the winner is not given" ,() => {
-    render(<StatusBar currentPlayer={CellState.stateO} winner={CellState.emptyState} />, container);
-    expect(container?.textContent).toBe("next: O");
+    render(<StatusBar currentPlayer={CellState.stateO} winner={CellState.emptyState} />);
+    expect(screen.getByText("next: O")).toBeInTheDocument();
   });
+
   it("should show the current player X when the winner is not given" ,() => {
-    render(<StatusBar currentPlayer={CellState.stateX} winner={CellState.emptyState} />, container);
-    expect(container?.textContent).toBe("next: X");
-  })
+    render(<StatusBar currentPlayer={CellState.stateX} winner={CellState.emptyState} />);
+        expect(screen.getByText("next: X")).toBeInTheDocument();
+  });
 });
